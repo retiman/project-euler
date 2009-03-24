2cb63f335215b83d516271222f5dbe8e381062e4
Min Huang <min.huang@alumni.usc.edu>
2009-03-23 21:48:05 -0700

    There's no need for the extra conversions

--- scala/problem-00004.scala
+++ scala/problem-00004.scala
@@ -5,12 +5,11 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 */
 import scala.Math._
 
-def is_palindrome(n: Int) = n.toString.reverse.toString == n.toString
+def is_palindrome(n: Int) = n.toString.reverse.toInt == n
 var best = 0
 
-val n = 999
-(n to 1 by -1).foreach(i => {
-  (n to 1 by -1).foreach(j => {
+(999 to 1 by -1).foreach(i => {
+  (999 to 1 by -1).foreach(j => {
     val x = i * j
     if (is_palindrome(x) && x > best)
       best = x
