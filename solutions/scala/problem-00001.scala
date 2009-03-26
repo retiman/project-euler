dd735fb574c9541edb0ef16d470f6877280d6e37
Min Huang <min.huang@alumni.usc.edu>
2009-03-26 00:41:05 -0700

    More description name

--- solutions/scala/problem-00001.scala
+++ solutions/scala/problem-00001.scala
@@ -1,2 +1,2 @@
-val s = (1 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).reduceLeft(_ + _)
-println(s)
+val sum = (1 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).reduceLeft(_ + _)
+println(sum)
