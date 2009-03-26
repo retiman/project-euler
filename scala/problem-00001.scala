7d4a87e7f3a8bb91d63b21753b5080f34e26075f
Min Huang <min.huang@alumni.usc.edu>
2009-03-25 23:53:05 -0700

    Shorter answer

--- scala/problem-00001.scala
+++ scala/problem-00001.scala
@@ -4,8 +4,5 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-val n = 1000
-val a = (0 to n - 1 by 3).reduceLeft[Int](_ + _)
-val b = (0 to n - 1 by 5).reduceLeft[Int](_ + _)
-val c = (0 to n - 1 by 15).reduceLeft[Int](_ + _)
-println(a + b - c)
+val s = (1 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).reduceLeft(_ + _)
+println(s)
