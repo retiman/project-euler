d63e30d38cc88889d702410e87759235b12fe823
Min Huang <min.huang@alumni.usc.edu>
2009-05-08 02:35:38 -0700

    Changed to camel case

--- solutions/problem-00004.scala
+++ solutions/problem-00004.scala
@@ -1,12 +1,17 @@
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
 import scala.Math._
 
-def is_palindrome(n: Int) = n.toString.reverse.toInt == n
+def isPalindrome(n: Int) = n.toString.reverse.toInt == n
 var best = 0
 
 (999 to 1 by -1).foreach(i => {
   (999 to 1 by -1).foreach(j => {
     val x = i * j
-    if (is_palindrome(x) && x > best)
+    if (isPalindrome(x) && x > best)
       best = x
   })
 })
