fcff7908592e1ce1c8fc898f1c26626a2902826b
Min Huang <min.huang@alumni.usc.edu>
2009-05-14 00:20:02 -0700

    Small bugfix

--- scala/problem-00015.scala
+++ scala/problem-00015.scala
@@ -7,7 +7,7 @@ import scala.Math._
 
 def fact(n: BigInt): BigInt = {
   def recur(n: BigInt, acc: BigInt): BigInt = if (n == 1) acc else recur(n - 1, n * acc)
-  recur(n, 1)
+  if (n == 0) 1 else recur(n, 1)
 }
 
 // View problem as a multipermutation on 20 L's and 20 R's where L means "go left" and R means "go right"
--- scala/problem-00020.scala
+++ scala/problem-00020.scala
@@ -7,7 +7,7 @@ import scala.Math._
 
 def fact(n: BigInt): BigInt = {
   def recur(n: BigInt, acc: BigInt): BigInt = if (n == 1) acc else recur(n - 1, n * acc)
-  recur(n, 1)
+  if (n == 0) 1 else recur(n, 1)
 }
 
 val sum = fact(100).toString.map(_.toString.toInt).reduceLeft(_+_)
