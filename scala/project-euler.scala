11b511c36c42d2460d763c603eccefde3d20d94d
Min Huang <min.huang@alumni.usc.edu>
2009-05-24 19:50:20 -0700

    Fixed bug with GCD, added rho factorization

--- scala/project-euler.scala
+++ scala/project-euler.scala
@@ -1,8 +1,27 @@
+import scala.util.Random
 import scala.Math._
+
+val rand = new Random
 
 implicit def tuple2Wrapper[A, B](t: Pair[A, B]) = new {
   def a = t._1
   def b = t._2
+}
+
+def gcd(a: Long, b: Long): Long = (a, b) match {
+  case (0, _) => b
+  case (_, 0) => a
+  case (_, _) => gcd(b, a % b)
+}
+
+def lcm(a: Long, b: Long) = a * (b / gcd(a, b))
+
+def mpow(base: Int, exp: Int)(mod: Int) = {
+  def recur(a: Int, b: Int, e: Int): Int = if (e <= 0) a else {
+    val t = if ((e & 1) == 1) (a * b) % mod else a
+    recur(t, (b * b) % mod, e >> 1)
+  }
+  recur(1, base, exp)
 }
 
 def fermatFactors(n: Long): Set[Long] = {
@@ -18,12 +37,23 @@ def fermatFactors(n: Long): Set[Long] = {
   Set(a - s, a + s).map(_.toLong)
 }
 
-def primeFactors(n: Long): Set[Long] = {
-  val factors = fermatFactors(n)
+def rhoFactors(n: Long) = {
+  def f(x: Long) = x * x + 1
+  def recur(x: Long, y: Long, d: Long): Long = if (d != 1) d else {
+    val (a, b) = (f(x) % n, f(f(y)) % n)
+    val c = gcd(abs(a-b), n)
+    recur(a, b, c)
+  }
+  val factor = recur(2, 2, 1)
+  Set(factor, n / factor)
+}
+
+def primeFactors(n: Long)(f: Long => Set[Long]): Set[Long] = {
+  val factors = f(n)
   if (factors contains 1)
     factors - 1
   else
-    factors.map(f => primeFactors(f)).reduceLeft(_ ++ _)
+    factors.map(factor => primeFactors(factor)(f)).reduceLeft(_ ++ _)
 }
 
 def divisors(n: Int) = {
@@ -40,14 +70,6 @@ def order(a: Int, m: Int) = {
   recur(a % m, (1 to m - 1).toList)
 }
 
-def gcd(a: Long, b: Long): Long = (a, b) match {
-  case (0, _) => a
-  case (_, 0) => b
-  case (_, _) => gcd(b, a % b)
-}
-
-def lcm(a: Long, b: Long) = a * (b / gcd(a, b))
-
 def tau(n: Int) = if (n == 1) 1 else 2 * (1 to sqrt(n)).filter(n % _ == 0).length
 
 def sigma(n: Int) = divisors(n).reduceLeft(_ + _)
@@ -56,13 +78,4 @@ def relprime(n: Int) = (2 to n - 1).filter(gcd(n, _) == 1)
 
 def totient(n: Int) = relprime(n).length
 
-def mpow(base: Int, exp: Int)(mod: Int) = {
-  def recur(a: Int, b: Int, e: Int): Int = e match {
-    case e if e > 0 => {
-      val t = if ((e & 1) == 1) (a * b) % mod else a
-      recur(t, (b * b) % mod, e >> 1)
-    }
-    case _ => a
-  }
-  recur(1, base, exp)
-}
+
