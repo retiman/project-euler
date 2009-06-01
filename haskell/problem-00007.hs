915559103fd2efe728dbea22fa21b271bec89273
Min Huang <min.huang@alumni.usc.edu>
2009-05-31 19:02:46 -0700

    Added problem 7

---
+++ haskell/problem-00007.hs
@@ -0,0 +1,25 @@
+{-
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+-}
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs@(x:xt) ys@(y:yt) = case compare x y of
+    LT -> x : (merge xt ys)
+    EQ -> x : (merge xt yt)
+    GT -> y : (merge xs yt)
+
+remove :: (Ord a) => [a] -> [a] -> [a]
+remove xs@(x:xt) ys@(y:yt) = case compare x y of
+    LT -> x : (remove xt ys)
+    EQ -> remove xt yt
+    GT -> remove xs yt
+
+primes, composites :: [Integer]
+primes = [2,3,5] ++ (remove [7,9..] composites)
+composites = foldr1 f . map g . tail $ primes
+    where f (x:xs) ys = x : (merge xs ys)
+          g p = [n*p | n <- [p,p+2..]]
+
+result = last . take 10001 $ primes
+main = print result
