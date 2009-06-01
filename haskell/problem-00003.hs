9da54af55d4d44ef4b93fb96c099979b2d61194b
Min Huang <min.huang@alumni.usc.edu>
2009-05-31 19:02:21 -0700

    Better performing solution

--- haskell/problem-00003.hs
+++ haskell/problem-00003.hs
@@ -3,25 +3,26 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 -}
-merge :: (Ord a) => [a] -> [a] -> [a]
-merge xs@(x:xt) ys@(y:yt) = case compare x y of
-    LT -> x : (merge xt ys)
-    EQ -> x : (merge xt yt)
-    GT -> y : (merge xs yt)
+import Data.List
 
-remove :: (Ord a) => [a] -> [a] -> [a]
-remove xs@(x:xt) ys@(y:yt) = case compare x y of
-    LT -> x : (remove xt ys)
-    EQ -> remove xt yt
-    GT -> remove xs yt
+f :: Integer -> Integer
+f x = x^2+1
 
-primes, composites :: [Integer]
-primes = [2,3,5] ++ (remove [7,9..] composites)
-composites = foldr1 f . map g . tail $ primes
-    where f (x:xs) ys = x : (merge xs ys)
-          g p = [n*p | n <- [p,p+2..]]
+factors' :: Integer -> Integer -> Integer -> Integer -> Integer
+factors' x y d n | d > 1 = d
+factors' x y d n = factors' a b (gcd c n) n
+    where a = mod (f x) n
+          b = mod (f . f $ y) n
+          c = abs (a-b)
 
-result = head [p | p <- reverse . takeWhile (<s) $ primes, n `mod` p == 0]
-         where n = 600851475143
-               s = floor . sqrt . fromIntegral $ n
+factors :: Integer -> [Integer]
+factors n = [factor, n `div` factor]
+    where factor = factors' 2 2 1 n
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = case factors n of
+    xs | elem 1 xs -> xs
+    xs -> nub . foldr1 (++) . map primeFactors $ xs
+
+result = maximum . primeFactors $ 600851475143
 main = print result
