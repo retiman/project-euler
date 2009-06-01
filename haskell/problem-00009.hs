df48aeb639ad60136d52396aefafe8640b69d5f4
Min Huang <min.huang@alumni.usc.edu>
2009-05-31 20:14:14 -0700

    Solutions for 9 and 10 (10 is slow

---
+++ haskell/problem-00009.hs
@@ -0,0 +1,16 @@
+{-
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+-}
+triple = head [[a,b,c] | a <- [1..332], 
+                         b <- [a..((1000-a) `div` 2)-1], 
+                         c <- [1000-a-b],
+                         a+b+c == 1000,
+                         a^2+b^2 == c^2]
+result = product triple
+main = print result                        
---
+++ haskell/problem-00010.hs
@@ -0,0 +1,25 @@
+{-
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+-}
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs@(x:xt) ys@(y:yt) = case compare x y of
+    LT -> x : (merge xt ys)
+    EQ -> x : (merge xt yt)
+    GT -> y : (merge xs yt)
+
+remove :: (Ord a) => [a] -> [a] -> [a]
+remove xs@(x:xt) ys@(y:yt) = case compare x y of
+    LT -> x : (remove xt ys)
+    EQ -> remove xt yt
+    GT -> remove xs yt
+
+primes, composites :: [Integer]
+primes = [2,3,5] ++ (remove [7,9..] composites)
+composites = foldr1 f . map g . tail $ primes
+    where f (x:xs) ys = x : (merge xs ys)
+          g p = [n*p | n <- [p,p+2..]]
+
+result = sum . takeWhile (<2*10^6) $ primes
+main = print result
