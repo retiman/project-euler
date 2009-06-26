ffba7a4c72c2184f8a70caedd705b76ecb4f0478
Min Huang <min.huang@alumni.usc.edu>
2009-06-26 01:27:35 -0700

    Better performing solution

--- solutions/scala/problem-00014.scala
+++ solutions/scala/problem-00014.scala
@@ -2,35 +2,31 @@
 [minhuang@mocha:scala]$ time scala problem-00014.scala 
 837799
 
-real  0m10.598s
-user  0m9.441s
-sys 0m0.300s
+real    0m7.191s
+user    0m7.280s
+sys     0m0.264s
 */
 import scala.collection.mutable.HashMap
 
-val map = HashMap[Int, Long](1 -> 1)
+val map = HashMap[Long, Long](1L -> 1L)
 val limit = 1000000
 
-def chainLength(n: Long) = {
-  def next(n: Long) = if (n%2 == 0) n/2 else 3*n+1
-  def recur(n: Long, count: Long): Long = if (n == 1) count else recur(next(n), count+1)
-  recur(n, 1)
+def length(n: Long): Long = n match {
+  case 1                   => 1
+  case n if map contains n => map(n)
+  case n if n%2 == 0       => 1 + length(n/2)
+  case _                   => 1 + length(3*n+1)
 }
 
-for (n <- 2 to limit if !(map contains n)) {
-  map(n) = chainLength(n)
-  // Further powers of 2 are already calculated by this calculation
-  def mark(i: Int, count: Long): Unit = if (i <= limit) {
-    map(i) = count+1
-    mark(2*i, count+1)
-  }  
-  mark(2*n, map(n))
+def f(n: Long) = {
+  if (!(map contains n)) 
+    map(n) = length(n)
+  (n, map(n))
 }
 
-implicit def pairWrapper(a: Pair[Int, Long]) = new {
-  def max(b: Pair[Int, Long]) = if (a._2 > b._2) a else b
+implicit def pairWrapper(a: Pair[Long, Long]) = new {
+  def max(b: Pair[Long, Long]) = if (a._2 > b._2) a else b
 }
-
-val max = map.reduceLeft(_ max _)._1
-
-println(max)
+                  
+val result = (1 to limit).map(_.toLong).map(f _).reduceLeft(_ max _)._1
+println(result)
