3dcd73505ea7929f7bc08ee2e19adf7f5fd17d17
Min Huang <min.huang@alumni.usc.edu>
2009-07-10 14:42:22 -0700

    Am I a haskell programmer?  Just look at me generate that fibonacci sequence

--- solutions/scala/problem-00002.scala
+++ solutions/scala/problem-00002.scala
@@ -1,18 +1,27 @@
 /*
 [minhuang@mocha:scala]$ time scala problem-00002.scala 
-4613732.0
+4613732
 
-real  0m1.289s
-user  0m0.256s
-sys 0m0.052s
+real    0m2.045s
+user    0m0.376s
+sys     0m0.060s
 */
 import scala.Math._
 import scala.Stream._
 
-val phi = (1.0 + sqrt(5.0)) / 2.0
-def fib(n: Int) = floor(pow(phi, n) / pow(5.0, .5) + .5)
-val result = from(0, 1).map(fib(_))
-                       .filter(_ % 2 == 0)
-                       .takeWhile(_ <= 4E6)
-                       .reduceLeft(_ + _)
+
+/*
+// Ouch! This does not work as of scala 2.7.4 as the following code gives a
+// compiler exception:
+// lazy val x = 0 :: from(1)
+implicit def streamExtras[Int](s: Stream[Int]) = new {
+  def ::(n: Int) = cons(n, s)
+}
+*/
+implicit def streamExtras[T](s: Stream[T]) = new {
+  def zipWith(f: (T, T) => T, t: Stream[T]) = s.zip(t).map(a => f(a._1, a._2))
+}
+
+lazy val fibs: Stream[Int] = cons(0, cons(1, fibs.zipWith(_+_, fibs.tail)))
+val result = fibs.takeWhile(_ < 4000000).filter(_ % 2 == 0).reduceLeft(_ + _)
 println(result)
