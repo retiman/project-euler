e3d30250121c1ca7fbee812ab9f137b1410a2761
Min Huang <min.huang@alumni.usc.edu>
2009-07-10 13:39:37 -0700

    A solution is forthcoming, but slow as molasses

--- solutions/haskell/problem-00014.hs
+++ solutions/haskell/problem-00014.hs
@@ -1,25 +1,22 @@
 {-
-Wow.. 5 hours and no solution.  I'm bad at haskell.
+[minhuang@mocha:haskell]$ time runghc problem-00014.hs 
+837799
+
+real  10m52.592s
+user  10m47.868s
+sys 0m0.156s
 -}
-import Prelude hiding (lookup)
-import Control.Monad
-import Data.HashTable
 import Data.List (foldl')
 
-collatz :: (HashTable Int Int) -> Int -> IO Int
-collatz m n = lookup m n >>= f'
-              where f' (Just v) = return v
-                    f' Nothing | even n = collatz m (div n 2) >>= return . (+1)
-                    f' _ = collatz m (3*n+1) >>= return . (+1)
+collatz :: Int -> Integer
+collatz 1 = 1
+collatz n | even n = 1 + collatz (div n 2)
+collatz n | otherwise = 1 + collatz (3*n+1)
 
-max' :: (Int, Int) -> (Int, Int) -> (Int, Int)
 max' a b | snd a > snd b = a
 max' a b | otherwise = b
 
-main = do m <- fromList hashInt [(1, 1)]
-          forM_ [1..limit] $ \n -> do v <- collatz m n
-                                      insert m n v
-          l <- toList m
-          let result = foldl' max' (0,0) l
-          print result
-          where limit = 1000000
+alist = [(n, collatz n) | n <- [1..1000000]]
+best = foldl' max' (0,0) alist
+
+main = print . fst $ best
