0ffa142c66bbbbad93ba5a0e10fa6daa175d7306
Min Huang <min.huang@alumni.usc.edu>
2009-07-10 17:33:40 -0700

    So this is how you fold without naming args

--- solutions/ruby/problem-00001.rb
+++ solutions/ruby/problem-00001.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
-result = (1..999).find_all { |x| x % 3 == 0 || x % 5 == 0 }.inject(0) { |acc, x| acc + x }
+# [minhuang@mocha:ruby]$ time ruby problem-00001.rb 
+# 233168
+#
+# real    0m0.016s
+# user    0m0.012s
+# sys     0m0.008s
+result = (1..999).find_all { |x| x % 3 == 0 || x % 5 == 0 }.inject(:+)
 puts result
--- solutions/ruby/problem-00002.rb
+++ solutions/ruby/problem-00002.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+# [minhuang@mocha:ruby]$ time ruby problem-00002.rb 
+# 4613732
+#
+# real    0m0.011s
+# user    0m0.004s
+# sys     0m0.004s
 curr, succ, acc = 0, 1, 0
 while curr < 4000000
   curr, succ = succ, curr + succ
