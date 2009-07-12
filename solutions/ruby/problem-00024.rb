bc22cafdd0f6ab9b69aa6249d7da7809c87e0a4e
Min Huang <min.huang@alumni.usc.edu>
2009-07-11 22:36:35 -0700

    Problem #24 in Ruby

---
+++ solutions/ruby/problem-00024.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# [minhuang@mocha:ruby]$ time ruby problem-00024.rb 
+# 2783915460
+#
+# real  0m0.022s
+# user  0m0.012s
+# sys 0m0.008s
+
+$limit = 1_000_000
+
+def fact(n)
+  (1..n).inject(&:*)
+end
+
+def compute(list, curr)
+  permutations = fact(list.length - 1)
+  index = ($limit - curr) / permutations
+  digit = list[index]
+  succ = curr + index * permutations
+  if list.length == 2 && $limit - curr == 0
+    return list
+  end
+  if list.length == 2 && $limit - curr == 1
+    return list.reverse
+  end
+  return [digit] + compute(list.delete_if { |x| x == digit }, succ)
+end
+  
+puts compute((0..9).to_a, 1).join.to_i
