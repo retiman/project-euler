01354d167c5bc25c5ad9a8eaa55fb04e432f19b4
Min Huang <min.huang@alumni.usc.edu>
2009-07-11 22:59:20 -0700

    Problem #25 in Ruby

---
+++ solutions/ruby/problem-00025.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# [minhuang@mocha:ruby]$ time ruby problem-00025.rb 
+# 4782
+#
+# real  0m1.719s
+# user  0m1.676s
+# sys 0m0.012s
+i, curr, succ = 0, 0, 1
+while curr.to_s.length < 1000
+  i, curr, succ = i + 1, succ, curr + succ
+end
+puts i
