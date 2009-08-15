681f0224d81d43fa0c2592238f4b0852aca4f0cd
Min Huang <min.huang@alumni.usc.edu>
2009-08-14 22:18:08 -0700

    No need for such complications in only one problem

--- solutions/scala/problem-00188.scala
+++ solutions/scala/problem-00188.scala
@@ -2,35 +2,17 @@
 [minhuang@mocha:scala]$ time scala problem-00188.scala 
 95962097
 
-real    0m3.536s
-user    0m1.648s
-sys     0m0.088s
+real    0m3.627s
+user    0m0.524s
+sys     0m0.052s
 */
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.HashSet
-import scala.io.Source
-import scala.Math
-
-val primes = Source.fromFile("../../data/primes.txt").getLines.map(_.trim.toLong).toList
-
-def isqrt(n: Long) = Math.floor(Math.sqrt(n)).toLong
-
-def pfactors(n: Long) = primes.takeWhile(_ <= n).filter(n % _ == 0)
-
-def divisors(n: Long): Set[Long] = {
-  var ts = new HashSet[Long]()
-  var t = 2L
-  while (t <= isqrt(n)) {
-    if (n % t == 0) ts += t
-    t += 1
+def order(a: Long, m: Long) = {
+  def recur(b: Long, n: Long): Long = n match {
+    case n if n == m - 1 => n
+    case n if b == 1     => n
+    case _               => recur((a * b) % m, n + 1)
   }
-  Set[Long]() ++ ts ++ ts.map(n / _) ++ Set(1L)
-}
-
-def totient(n: Long) = {
-  val ps = pfactors(n)
-  val xs = ps.map(_ - 1).zip(ps)
-  xs.foldLeft(n) { (a, b) => a * b._1 / b._2 }
+  recur(a % m, 1L)
 }
 
 def mexp(b: Long, e: Long)(m: Long) = {
@@ -41,18 +23,10 @@ def mexp(b: Long, e: Long)(m: Long) = {
   recur(1, b, e)
 }
 
-def order(a: Long, m: Long): Long = {
-  val phi = totient(m)
-  val ds = divisors(phi) + phi
-  ds.toList.sort(_ < _).dropWhile(mexp(a, _)(m) != 1).first
-}
-
 def mtet(b: Long, e: Long)(m: Long): Long = {
-  val r = b % m
-  (r, e, m) match {
-    case (0, _, _) => 0
-    case (_, _, 1) => 0
-    case (_, 1, _) => r
+  (e, m) match {
+    case (_, 1) => 0
+    case (1, _) => b % m
     case _      => val ord = order(b, m)
                    val t = mtet(b, e - 1)(ord)
                    mexp(b, t)(m)
