fec92914315fee69a253bdc09e3a231827d3807a
Min Huang <min.huang@alumni.usc.edu>
2009-08-14 23:14:42 -0700

    Problem 188 in Ruby; 10x as slow as Scala

---
+++ solutions/ruby/problem-00188.rb
@@ -0,0 +1,36 @@
+# [minhuang@mocha:ruby]$ time ruby problem-00188.rb 
+# 95962097
+#
+# real    0m5.693s
+# user    0m5.628s
+# sys     0m0.016s
+def order(a, m)
+  b, e = a % m, 1
+  while (b != 1)
+    b = (a * b) % m
+    e += 1
+  end
+  e
+end
+
+def mexp(a, e, m)
+  r, b, k = 1, a, e
+  while (k > 0)
+    r = (r * b) % m if (k & 1 == 1) 
+    k >>= 1
+    b = (b * b) % m
+    return 0 if b == 0
+    return r if b == 1
+  end
+  r
+end
+
+def mtet(a, t, m)
+  return 0 if m == 1
+  return a % m if t == 1
+  ord = order(a, m)
+  e = mtet(a, t - 1, ord)
+  mexp(a, e, m)
+end
+
+puts mtet(1777, 1855, 100_000_000)
