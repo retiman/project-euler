801464ed83abffe741e4108700d5640e4cc4f72a
Min Huang <min.huang@alumni.usc.edu>
2010-06-13 22:31:14 -0700

    Consistent formatting

--- src/javascript/problem-00001.js
+++ src/javascript/problem-00001.js
@@ -4,11 +4,13 @@
 // real 0m0.009s
 // user 0m0.008s
 // sys  0m0.004s
-function range(begin, end) { for (let i = begin; i < end; i++) yield i; }
+function range(begin, end) {
+  for (let i = begin; i < end; i++) yield i;
+}
 
 let xs = range(1, 1000);
 let result = [x for each (x in xs) if (x % 3 === 0 || x % 5 === 0)].
-    reduce(function (a, b) {
-      return a + b;
-    });
+  reduce(function (a, b) {
+    return a + b;
+  });
 print(result);
