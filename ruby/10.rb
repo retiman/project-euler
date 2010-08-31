79d42735d5277e5298597807c5c351444566887a
Min Huang <min.huang@alumni.usc.edu>
2010-08-30 22:54:49 -0700

    Unfortunately, using Ruby's standard library for this is rather slow

--- ruby/10.rb
+++ ruby/10.rb
@@ -1,8 +1,20 @@
-# [minhuang@mocha:ruby]$ time ruby problem-00010.rb 
+# time ruby 10.rb
 # 142913828922
-#
-# real    2m8.094s
-# user    1m21.453s
-# sys     0m45.147s
-require 'mathn'
-puts Prime.new.take_while { |p| p < 2000000}.inject(&:+)
+# 13.89user 3.00system 0:17.11elapsed 98%CPU (0avgtext+0avgdata 0maxresident)k
+# 0inputs+0outputs (0major+3364minor)pagefaults 0swaps
+
+def sieve(limit)
+  current = 2
+  numbers = 0.upto(limit).to_a
+  numbers[1] = 0
+  while (current < limit)
+    (2 * current .. limit).step(current).each { |i| numbers[i] = 0 }
+    begin
+      current += 1
+    end while current <= limit && numbers[current] == 0
+  end
+  numbers.select { |n| n != 0 }
+end
+
+result = sieve(2_000_000).inject(&:+)
+puts result
