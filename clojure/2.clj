832b640a51ebbbe6433d6aeeec5120f804777bce
Min Huang <mhuang@attinteractive.com>
2010-09-03 17:05:29 -0700

    Solution for #2 in Clojure; finally figured out the guts of the lazy cat

---
+++ clojure/2.clj
@@ -0,0 +1,7 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 2.clj
+; 4613732
+; 0.97user 0.04system 0:00.65elapsed 156%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+64outputs (1major+13834minor)pagefaults 0swaps
+
+(def fibs (lazy-cat '(0 1) (map + fibs (rest fibs))))
+(println (reduce + (filter even? (take-while #(< % 4000000) fibs))))
