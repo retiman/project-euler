70d7ba1a073d2956ec15a14f1b0fb02dfe44589f
Min Huang <mhuang@attinteractive.com>
2010-09-07 14:11:59 -0700

    Problem #5 in Clojure

---
+++ clojure/5.clj
@@ -0,0 +1,16 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 5.clj
+; 232792560
+; 0.89user 0.03system 0:00.65elapsed 143%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+64outputs (1major+13974minor)pagefaults 0swaps
+
+(defn gcd [a b & more]
+  (if more
+    (reduce gcd (conj more (gcd a b)))
+    (if (= b 0) a (gcd b (rem a b)))))
+
+(defn lcm [a b & more]
+  (if more
+    (reduce lcm (conj more (lcm a b)))
+    (* a (/ b (gcd a b)))))
+
+(println (reduce lcm (range 1 20)))
