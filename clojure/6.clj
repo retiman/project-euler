2da84c3579135ab2e1f6f04cf566e5d9955d953d
Min Huang <mhuang@attinteractive.com>
2010-09-07 14:47:32 -0700

    Problem #6 in Clojure

---
+++ clojure/6.clj
@@ -0,0 +1,9 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 6.clj
+; 25164150
+; 0.98user 0.04system 0:00.65elapsed 154%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+64outputs (1major+13946minor)pagefaults 0swaps
+
+(let [a (reduce + (map #(* % %) (range 1 101)))
+      b (reduce + (range 1 101))
+      r (- (* b b) a)]
+  (println r))
