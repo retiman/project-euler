c9b1a075786fdb8994a02b44458051a7ee6a57e5
Min Huang <mhuang@attinteractive.com>
2010-09-07 13:03:50 -0700

    Stupid Clojure.  Take this!

--- clojure/3.clj
+++ clojure/3.clj
@@ -1,10 +1,21 @@
 ; JAVA_OPTS="-Xmx1024M" time clojure 3.clj
-; 6857.0
-; 12.56user 0.36system 0:11.81elapsed 109%CPU (0avgtext+0avgdata 0maxresident)k
-; 0inputs+0outputs (0major+50162minor)pagefaults 0swaps
+; 6857
+; 9.46user 0.34system 0:07.39elapsed 132%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+64outputs (1major+139470minor)pagefaults 0swaps
 
 (use 'clojure.set)
 (use 'clojure.contrib.math)
+
+; Here's a fun thing about Clojure:
+; user=> (contains? #{1.0} 1)
+; false
+; user=> (contains? #{1.0} (double 1))
+; true
+; user=> (contains? #{(long 1)} 1)
+; false
+(defn in? [s e]
+  "Does what I really want 'contains?' to do"
+  (some #(= e %) s))
 
 (defn fermat-factors [n]
   (if (= 0 (rem n 2))
@@ -16,18 +27,9 @@
           #{(- a s) (+ a s)}
           (recur (inc a)))))))
 
-; Here's a fun thing about Clojure:
-; user=> (contains? #{1.0} 1)
-; false
-; user=> (contains? #{1.0} (double 1))
-; true
-; user=> (contains? #{(long 1)} 1)
-; false
-; So unless I want to put casts *everywhere*, I think I have to make the
-; factors simply a set of ints.
 (defn prime-factors [n]
-  (let [factors (set (map int (fermat-factors n)))]
-    (if (contains? factors 1)
+  (let [factors (fermat-factors n)]
+    (if (in? factors 1)
       (disj factors 1)
       (reduce union (map prime-factors factors)))))
 
