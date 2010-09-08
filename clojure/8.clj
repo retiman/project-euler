77825711e1a9b8e940c67ad5dc89041a8ce3d24a
Min Huang <mhuang@attinteractive.com>
2010-09-07 18:11:22 -0700

    Problem #8 in Clojure

---
+++ clojure/8.clj
@@ -0,0 +1,15 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 8.clj
+; 40824
+; 1.16user 0.06system 0:00.81elapsed 151%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+64outputs (1major+19958minor)pagefaults 0swaps
+
+(use '[clojure.contrib.str-utils2 :only (split-lines)])
+
+(def parse-int #(Integer/parseInt (str %)))
+
+(def s (apply str (split-lines (slurp "../data/8.txt"))))
+
+(println
+  (apply max
+    (for [i (range 1 (- (count s) 4))]
+      (reduce * (map #(parse-int (get s %)) (range i (+ i 5)))))))
