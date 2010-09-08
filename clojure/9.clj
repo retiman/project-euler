c3d463c1992bd8ad099f877a1c3ecdb2bf4481e2
Min Huang <mhuang@attinteractive.com>
2010-09-07 18:18:08 -0700

    Problem #9 in Clojure

---
+++ clojure/9.clj
@@ -0,0 +1,13 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 9.clj
+; 31875000
+; 1.64user 0.09system 0:00.96elapsed 180%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+64outputs (1major+22320minor)pagefaults 0swaps
+
+(println
+  (first
+    (for [a (range 1 333)
+          b (range a (/ (- 1000 a) 2))
+          :let [c (- 1000 a b)]
+          :when (= (+ (* a a) (* b b))
+                   (* c c))]
+      (* a b c))))
