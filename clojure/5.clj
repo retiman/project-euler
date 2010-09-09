817dd275d412c74bc018ea3ac2c421a4035ca9d3
Min Huang <min.huang@alumni.usc.edu>
2010-09-08 19:59:39 -0700

    There's no need to explicitly test for more in these cases

--- clojure/5.clj
+++ clojure/5.clj
@@ -3,14 +3,12 @@
 ; 0.89user 0.03system 0:00.65elapsed 143%CPU (0avgtext+0avgdata 0maxresident)k
 ; 0inputs+64outputs (1major+13974minor)pagefaults 0swaps
 
-(defn gcd [a b & more]
-  (if more
-    (reduce gcd (conj more (gcd a b)))
-    (if (= b 0) a (gcd b (rem a b)))))
+(defn gcd
+  ([a b] (if (= b 0) a (gcd b (rem a b))))
+  ([a b & more] (reduce gcd (conj more (gcd a b)))))
 
-(defn lcm [a b & more]
-  (if more
-    (reduce lcm (conj more (lcm a b)))
-    (* a (/ b (gcd a b)))))
+(defn lcm
+  ([a b] (* a (/ b (gcd a b))))
+  ([a b & more] (reduce lcm (conj more (lcm a b)))))
 
 (println (reduce lcm (range 1 20)))
