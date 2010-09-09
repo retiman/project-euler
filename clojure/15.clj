d5bfdcd4dc8470dacdf8747eb9d966d5974f1d1b
Min Huang <min.huang@alumni.usc.edu>
2010-09-08 20:13:12 -0700

    Even so, this is even shorter

--- clojure/15.clj
+++ clojure/15.clj
@@ -3,12 +3,7 @@
 ; 0.90user 0.02system 0:00.65elapsed 141%CPU (0avgtext+0avgdata 0maxresident)k
 ; 0inputs+64outputs (1major+13952minor)pagefaults 0swaps
 
-(defn fact [n]
-  (let [f (fn [n acc]
-            (if (or (zero? n) (= n 1))
-              acc
-              (recur (dec n) (* n acc))))]
-    (f n 1)))
+(defn fact [n] (reduce * (range 1 n)))
 
 (let [n (fact (+ 20 20))
       d (* (fact 20) (fact 20))
