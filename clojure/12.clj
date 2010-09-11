36e65915675f6f421bb8913a28d9593043e9c8f3
Min Huang <min.huang@alumni.usc.edu>
2010-09-11 09:53:43 -0700

    Problem #12 in Clojure

---
+++ clojure/12.clj
@@ -0,0 +1,17 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 12.clj
+; 76576500
+; 15.98user 0.44system 0:13.62elapsed 120%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+0outputs (0major+62317minor)pagefaults 0swaps
+
+(use '[clojure.contrib.math :only (sqrt)])
+
+(defn tau [n]
+  (if (= n 1)
+    1
+    (* 2 (count (filter #(zero? (rem n %)) (range 1 (sqrt n)))))))
+
+(println
+  (loop [n 3 i 3]
+    (if (> (tau n) 500)
+      n
+      (recur (+ n i) (inc i)))))
