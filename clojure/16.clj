fc06340f5f51a201d59a0edfe42d20d842cf5893
Min Huang <min.huang@alumni.usc.edu>
2010-09-11 22:33:43 -0700

    Problem #16 in Clojure

---
+++ clojure/16.clj
@@ -0,0 +1,8 @@
+; JAVA_OPTS="-Xmx1024M" time clojure 16.clj
+; 1366
+; 1.32user 0.10system 0:01.45elapsed 99%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+0outputs (0major+10090minor)pagefaults 0swaps
+
+(use '[clojure.contrib.math :only (expt)])
+
+(println (reduce + (map #(Integer/parseInt (str %)) (str (expt 2 1000))))
