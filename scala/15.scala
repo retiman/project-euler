e518d8aef13328c49b52df2fa5b0819e329a54de
Min Huang <mhuang@attinteractive.com>
2010-09-14 19:23:04 -0700

    There's no good reason to recur here

--- scala/15.scala
+++ scala/15.scala
@@ -1,13 +1,9 @@
-// JAVA_OPTS="-Xmx1024M" time scala -deprecation 15.scala
-// 137846528820
-// 0.69user 0.04system 0:01.41elapsed 52%CPU (0avgtext+0avgdata 0maxresident)k
-// 0inputs+64outputs (1major+15738minor)pagefaults 0swaps
+// JAVA_OPTS="-Xmx1024M" time clojure 15.clj
+// 1378465288200
+// 0.84user 0.04system 0:00.70elapsed 124%CPU (0avgtext+0avgdata 0maxresident)k
+// 0inputs+64outputs (1major+13870minor)pagefaults 0swaps
 
-def fact(n: BigInt): BigInt = {
-  def recur(n: BigInt, acc: BigInt): BigInt = if (n == 1) acc
-                                              else recur(n - 1, n * acc)
-  if (n == 0) 1 else recur(n, 1)
-}
+def fact(n: BigInt): BigInt = (2 to n).reduceLeft(_ * _)
 
 val num = fact(20 + 20)
 val denom = fact(20) * fact(20)
