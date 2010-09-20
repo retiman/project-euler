587e87b4b46d6fec3900bd2457eface259629e0a
Min Huang <mhuang@attinteractive.com>
2010-09-20 14:51:16 -0700

    BigInt ranges in 2.8.0, but look at how ugly they are

--- scala/20.scala
+++ scala/20.scala
@@ -3,11 +3,6 @@
 // 0.66user 0.05system 0:02.37elapsed 30%CPU (0avgtext+0avgdata 0maxresident)k
 // 8inputs+64outputs (1major+16154minor)pagefaults 0swaps
 
-def fact(n: BigInt): BigInt = {
-  def recur(n: BigInt, acc: BigInt): BigInt = if (n == 1) acc
-                                              else recur(n - 1, n * acc)
-  if (n == 0) 1 else recur(n, 1)
-}
-
+def fact(n: BigInt): BigInt = (BigInt(2) to n).reduceLeft(_ * _)
 val sum = fact(100).toString.map(_.toString.toInt).reduceLeft(_+_)
 println(sum)
