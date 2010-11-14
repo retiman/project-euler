5f0fcca905852c0782bf832a7ff7e3ac4ae8d0bd
Min Huang <min.huang@alumni.usc.edu>
2010-11-14 15:10:46 -0800

    Updated for Scala 2.8.1

--- scala/15.scala
+++ scala/15.scala
@@ -1,10 +1,10 @@
-// JAVA_OPTS="-Xmx1024M" time clojure 15.clj
-// 1378465288200
-// 0.84user 0.04system 0:00.70elapsed 124%CPU (0avgtext+0avgdata 0maxresident)k
-// 0inputs+64outputs (1major+13870minor)pagefaults 0swaps
+// JAVA_OPTS="-Xmx1024M" time scala -deprecation 15.scala
+// 137846528820
+// 1.93user 0.22system 0:08.88elapsed 24%CPU (0avgtext+0avgdata 0maxresident)k
+// 0inputs+0outputs (0major+11204minor)pagefaults 0swaps
 
+implicit def int2bigint(n: Int) = BigInt(n)
 def fact(n: BigInt): BigInt = (2 to n).reduceLeft(_ * _)
-
 val num = fact(20 + 20)
 val denom = fact(20) * fact(20)
 println(num / denom)
