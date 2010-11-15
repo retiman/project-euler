e616a75f9166a8e25910d0dc92cdaaf6d23a3373
Min Huang <min.huang@alumni.usc.edu>
2010-11-14 21:56:45 -0800

    Problem #107 in Clojure

--- clojure/107.clj
+++ clojure/107.clj
@@ -1,23 +1,27 @@
+; JAVA_OPTS=""-Xmx1024M"" time clojure 107.clj
+; 259679.0
+; 8.03user 0.39system 0:06.87elapsed 122%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+16outputs (0major+22495minor)pagefaults 0swaps
+
 (require '[clojure.contrib.str-utils2 :as su])
 (require '[clojure.set :as s])
-(refer-clojure :exclude '(flatten))
 
 (def inf Double/POSITIVE_INFINITY)
 
-(def graph
-  (let [unsplit-lines (su/split-lines (slurp "../data/107.txt"))
-        split-lines (map #(su/split % #",") unsplit-lines)
-        parse-element #(if (= "-" %) inf (Double/parseDouble %))
-        rows (map #(map parse-element %) split-lines)]
-    (vec (map vec rows))))
+ (def graph
+   (let [unsplit-lines (su/split-lines (slurp "../data/107.txt"))
+         split-lines (map #(su/split % #",") unsplit-lines)
+         parse-element #(if (= "-" %) inf (Double/parseDouble %))
+         rows (map #(map parse-element %) split-lines)]
+     (vec (map vec rows))))
 
 (def vertex-set
   ((comp set range count) graph))
 
 (def total-cost
-  (let [sum-row (fn [row]
-                  (reduce + (filter #(not (= inf %)) row)))]
-    (reduce + (map sum-row graph))))
+  (let [sum-row (fn [row] (reduce + (filter #(not (= inf %)) row)))
+        sum-graph (reduce + (map sum-row graph))]
+    (/ sum-graph 2)))
 
 (defn cost
   "Returns the cost of connecting node u to v"
@@ -29,15 +33,16 @@
 (defn neighbors
   "Returns the nodes connected to u by an edge"
   [u]
-  (filter #(not (= inf (cost [u %])))
-          (range 0 (count graph))))
+  (set
+    (filter #(not (= inf (cost [u %])))
+            (range 0 (count graph)))))
 
 (defn frontier-edges
   "Returns the frontier edges"
   [vertices]
   (set
     (for [u vertices
-          v (neighbors u)]
+          v (s/difference (neighbors u) vertices)]
       #{u v})))
 
 (defn cheapest-edge
@@ -54,9 +59,9 @@
   [graph]
   (loop [vertices #{0}
          edges #{}]
-    (let [edge (cheapest-edge vertices edges)]
-      (if (= vertices vertex-set)
-        edges
+    (if (= vertices vertex-set)
+      edges
+      (let [edge (cheapest-edge vertices edges)]
         (recur (s/union vertices edge)
                (conj edges edge))))))
 
