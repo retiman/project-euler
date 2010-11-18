2518313e41c1933819cc452be81805d021e13966
Min Huang <min.huang@alumni.usc.edu>
2010-11-17 23:46:52 -0800

    Notes & solution

--- clojure/53.clj
+++ clojure/53.clj
@@ -1,10 +1,13 @@
+; 4075
+; 7.42user 0.36system 0:05.56elapsed 140%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+0outputs (0major+27329minor)pagefaults 0swaps
+
 (use '[clj-pelib.math :only (choose)])
 
-(def choices
-  (for [n (range 1 (inc 100))
-        r (range 1 (dec n))
-        :let [c (choose n r)]
-        :when (> c 1000000)]
-      c))
-
-(println (count choices))
+(println
+  (count
+    (for [n (range 1 (inc 100))
+          r (range 1 (dec n))
+          :let [c (choose n r)]
+          :when (> c 1000000)]
+        c)))
