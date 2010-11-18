47195a43dc32df53e6ff9e9067bbde55697024ed
Min Huang <mhuang@attinteractive.com>
2010-11-17 19:16:40 -0800

    Half assed way to get soln to #69

--- clojure/69.clj
+++ clojure/69.clj
@@ -1,14 +1,15 @@
-(use 'clj-pelib.math)
+(use '[clj-pelib.math :only (totient)])
 
-(def pairs
-  (for [n (range 2 (inc 1000000))]
-    (let [phi (totient n)
-          ratio (/ n phi)]
-      (do
-        (println [n ratio])
-        [n ratio]))))
-
+; Very interesting output; the best candidates have digits like
+; abcabc.  For example, 250250, 990990, and 510510 produce the
+; best ratios.  Also, 510510 is the best answer.
 (println
   (first
-    (reduce (fn [a b] (if (> (last a) (last b)) a b))
-            pairs)))
+    (reduce (fn [a b]
+              (if (> (last a) (last b))
+                (do
+                  (println a)
+                  a)
+                b))
+            (map (fn [n] [n (/ n (totient n))])
+                 (range 2 (inc 1000000))))))
