6b25487e4cdcbcdca1bae97e0434ef5ef4dc10dd
Min Huang <min.huang@alumni.usc.edu>
2010-11-20 14:01:48 -0800

    Attempt at #69

---
+++ clojure/69.clj
@@ -0,0 +1,17 @@
+; NOT A SOLUTION
+
+(use '[clj-pelib.math :only (totient)])
+
+; Very interesting output; the best candidates have digits like
+; abcabc.  For example, 250250, 990990, and 510510 produce the
+; best ratios.  Also, 510510 is the best answer.
+(println
+  (first
+    (reduce (fn [a b]
+              (if (> (last a) (last b))
+                (do
+                  (println a)
+                  a)
+                b))
+            (map (fn [n] [n (/ n (totient n))])
+                 (range 2 (inc 1000000))))))
