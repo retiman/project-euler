92848ad23e05e78698b77d7b9438b04e0c33935c
Min Huang <min.huang@alumni.usc.edu>
2010-11-21 04:19:27 -0800

    First stab at #41

---
+++ clojure/41.clj
@@ -0,0 +1,27 @@
+(require '[clojure.contrib.lazy-seqs :as seqs])
+
+(def best (atom 0))
+
+(defn pandigits [n]
+  (into #{}
+    (map char
+      (range (int \1) ((comp inc int first str) n)))))
+
+(def pandigits (memoize pandigits))
+
+(defn pandigital? [n]
+  (let [s (str n)
+        t (into #{} s)]
+    (do
+      (println "Checking" n ", best is" (deref best))
+      (and (= (count s) (count t))
+           (= t (pandigits (count s)))))))
+
+(loop [ps seqs/primes]
+  (let [p (first ps)]
+    (do
+      (when (pandigital? p)
+        (swap! best (fn [x] p)))
+      (if (> p 123456789)
+        (println (deref best))
+        (recur (rest ps))))))
