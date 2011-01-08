f576fa4dd58eefc92bc999d5d0120b8b913fdd65
Min Huang <min.huang@alumni.usc.edu>
2011-01-08 01:38:30 -0800

    This solution performs a bit better

--- clojure/41.clj
+++ clojure/41.clj
@@ -1,38 +1,26 @@
 ; 7652413
-; 1780.55user 102.25system 35:59.98elapsed 87%CPU (0avgtext+0avgdata 0maxresident)k
-; 144inputs+16outputs (3major+168277minor)pagefaults 0swaps
+; 137.54user 0.88system 2:21.36elapsed 97%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+24outputs (0major+106307minor)pagefaults 0swaps
 
-(require '[clojure.contrib.lazy-seqs :as seqs])
+(use '[clojure.contrib.lazy-seqs :only (primes)])
+(use '[clojure.contrib.combinatorics :only (permutations)])
+(use '[clojure.set :only (intersection)])
+(require '[clojure.contrib.str-utils2 :as su])
 
-(defn pandigits
-  "Returns a set of chars that comprise the digits of a 1 to n pandigital."
-  [n]
-  (into #{}
-    (map char
-      (range (int \1) ((comp inc int first str) n)))))
+(def parse-int #(Integer/parseInt %))
 
-(def pandigits (memoize pandigits))
+(def cs
+  (map char (range (int \1) ((comp inc int first str) 7))))
 
-(defn pandigital?
-  "Returns true if n is a pandigital; false otherwise."
-  [n]
-  (let [s (str n)
-        t (into #{} s)]
-    (do
-      (and (= (count s) (count t))
-           (= t (pandigits (count s)))))))
+; Only numbers up to 7654321 are considered.
+; Any pandigital containing digits 1 through 8 is not prime:
+; (apply + (range 1 9)) is 36 and thus divisible by 3
+; Any pandigital containing digits 1 through 9 is not prime:
+; (apply + (range 1 10)) is 45 and thus divisible by 3
+(def xs
+  (into #{} (take-while #(< % 7654321) primes)))
 
-; TODO:
-; This solution can stand to be optimized.  The answer is 7652413 through
-; verification with the Project Euler website, but it takes over an hour to
-; find it.
-(def best (atom 0))
+(def ys
+  (into #{} (map #(parse-int (su/join "" %)) (permutations cs))))
 
-(loop [ps seqs/primes]
-  (let [p (first ps)]
-    (do
-      (when (pandigital? p)
-        (swap! best (fn [x] p)))
-      (if (> p 123456789)
-        (println (deref best))
-        (recur (rest ps))))))
+(println (apply max (intersection xs ys)))
