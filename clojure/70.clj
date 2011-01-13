072ecdd49639be120ddd04287ceab0729b7e6fa2
Min Huang <min.huang@alumni.usc.edu>
2011-01-12 17:25:03 -0800

    As far as solutions go, it's pretty ugly, but it's a solution.

--- clojure/70.clj
+++ clojure/70.clj
@@ -1,25 +1,60 @@
-; NOT A SOLUTION
+; 8319823
+; 29.82user 0.95system 0:22.83elapsed 134%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+16outputs (0major+187686minor)pagefaults 0swaps
 
+(use 'clojure.contrib.combinatorics)
+(use 'clj-pelib.core)
 (use 'clj-pelib.math)
 
+(def primes
+  (load-primes "../data/primes.txt"))
+
+(def limit (int 1e7))
+
+; Search around the radius of (sqrt 1e7)
+(def ps
+  (take-while #(<= % (* 2 (inc (isqrt limit)))) primes))
+
+; Create pairs for semi-primes
+(def pairs
+  (combinations ps 2))
+
 (defn permutation?
-  "Returns true if a's digits are a permutation of b's digits; false
-  otherwise."
+  "Returns true if a's digits are a permutation of b's."
   [a b]
   (let [digits #(sort (into [] (str %)))]
     (= (digits a) (digits b))))
 
-(def pairs
-  (for [p (range 2 10000000)
-        :let [phi (dec p)]
-        :when (and (prime? p)
-                   (permutation? p phi))]
-    (let [pair [p (/ p phi)]]
-      (do
-        (println pair)
-        pair))))
+(defn phi
+  "Calculates the totient of a semi-prime created from p and q."
+  [p q]
+  (* (dec p) (dec q)))
 
-(println
-  "And the winner is..."
-  (reduce (fn [a b] (if (< (last a) (last b)) a b))
-          pairs))
+(defn f
+  "Creates a map entry of n/phi(n) -> [p q]."
+  [pair]
+  (let [p (first pair)
+        q (second pair)
+        n (* p q)]
+    [(/ n (phi p q)) [p q]]))
+
+(defn g
+  "Returns true if the totient of the semi-prime generated from [p q] is a
+  permutation of that semi-prime."
+  [pair]
+  (let [p (first pair)
+        q (second pair)
+        n (* p q)]
+    (permutation? n (phi p q))))
+
+; Create a map of n/phi(n) -> [p q], filtering out semi-primes who are
+; larger than 1e7
+(def h
+  (filter #(<= (apply * (second %)) 10000000)
+          (apply merge (cons {} (map f pairs)))))
+
+(let [entry (first (drop-while #(not (g (second %))) (sort h)))
+      pair (second entry)
+      p (first pair)
+      q (second pair)]
+  (println (* p q)))
