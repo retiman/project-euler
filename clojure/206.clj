60d103a687ba5cc215385252b2bc173a6e61da0d
Min Huang <min.huang@alumni.usc.edu>
2011-01-14 02:35:40 -0800

    Brute force solution to #206

---
+++ clojure/206.clj
@@ -0,0 +1,12 @@
+; 1389019170
+; 8.30user 0.42system 0:07.92elapsed 110%CPU (0avgtext+0avgdata 0maxresident)k
+; 0inputs+16outputs (0major+21759minor)pagefaults 0swaps
+
+(use 'clj-pelib.math)
+
+(println
+  (loop [n (inc (isqrt 19293949596979899))]
+    (let [k (* n n)]
+      (if (re-matches #"1\d2\d3\d4\d5\d6\d7\d8\d9" (str k))
+        (* n 10)
+        (recur (- n 2))))))
