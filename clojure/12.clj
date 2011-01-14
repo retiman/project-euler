825ecfa2572ca78f2f1e2102693f1f6cbd6f0448
Min Huang <min.huang@alumni.usc.edu>
2011-01-14 13:24:54 -0800

    Use tau from clj-pelib

--- clojure/12.clj
+++ clojure/12.clj
@@ -2,12 +2,8 @@
 ; 15.98user 0.44system 0:13.62elapsed 120%CPU (0avgtext+0avgdata 0maxresident)k
 ; 0inputs+0outputs (0major+62317minor)pagefaults 0swaps
 
+(use '[clj-pelib.math])
 (use '[clojure.contrib.math :only (sqrt)])
-
-(defn tau [n]
-  (if (= n 1)
-    1
-    (* 2 (count (filter #(zero? (rem n %)) (range 1 (sqrt n)))))))
 
 (println
   (loop [n 3 i 3]
