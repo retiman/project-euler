add1cf02a74d08a5919bc1e7bbd81e921a6f51b5
Min Huang <min.huang@alumni.usc.edu>
2012-02-21 23:48:17 -0800

    Indentation

--- racket/1.rkt
+++ racket/1.rkt
@@ -1,10 +1,10 @@
 (define (sum limit)
   (let recur ((i 0))
     (cond
-     ((>= i limit) 0)
-     ((zero? (modulo i 3)) (+ i (recur (+ i 1))))
-     ((zero? (modulo i 5)) (+ i (recur (+ i 1))))
-     (else (recur (+ i 1))))))
+      ((>= i limit) 0)
+      ((zero? (modulo i 3)) (+ i (recur (+ i 1))))
+      ((zero? (modulo i 5)) (+ i (recur (+ i 1))))
+      (else (recur (+ i 1))))))
 
 (display (sum 1000))
 (newline)
