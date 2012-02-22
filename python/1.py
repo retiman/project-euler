6fc40e14e5b8869322ac0c3ce3e2e78a3cf39b54
Min Huang <min.huang@alumni.usc.edu>
2012-02-21 23:33:45 -0800

    Don't need the try

--- python/1.py
+++ python/1.py
@@ -2,9 +2,5 @@
 # 0.04user 0.00system 0:00.05elapsed 94%CPU (0avgtext+0avgdata 7192maxresident)k
 # 0inputs+0outputs (0major+1902minor)pagefaults 0swaps
 
-try:
-  from functools import reduce
-except ImportError:
-  reduce = reduce
-
+from functools import reduce
 print(sum([n for n in range(1, 1000) if n % 3 == 0 or n % 5 == 0]))
