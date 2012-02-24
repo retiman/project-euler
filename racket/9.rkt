ec7dd7b643a2745624c4632f5e8eb69d5fb334c7
Min Huang <min.huang@alumni.usc.edu>
2012-02-24 00:52:18 -0800

    Don't have to define c twice

--- racket/9.rkt
+++ racket/9.rkt
@@ -2,14 +2,17 @@
 ; 0.27user 0.02system 0:00.30elapsed 99%CPU (0avgtext+0avgdata 39216maxresident)k
 ; 0inputs+0outputs (0major+12089minor)pagefaults 0swaps
 
-(display
-  (first
-    (for*/list
-      ((a (in-range 1 333))
-       (b (in-range a (/ (- 1000 a) 2)))
-       #:when (let ((c (- 1000 a b)))
-                (= (+ (* a a) (* b b))
-                   (* c c))))
-      (let ((c (- 1000 a b)))
-        (* a b c)))))
+(require racket/generator)
+
+(define g
+  (generator
+    ()
+    (for* ((a (in-range 1 333))
+           (b (in-range a (/ (- 1000 a) 2))))
+          (let ((c (- 1000 a b)))
+            (when (= (+ (* a a) (* b b))
+                     (* c c))
+              (yield (* a b c)))))))
+
+(display (g))
 (newline)
