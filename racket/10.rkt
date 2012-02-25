eb488718b90cd73dd86034b822c67467157954f8
Min Huang <min.huang@alumni.usc.edu>
2012-02-24 22:14:03 -0800

    Times

--- racket/10.rkt
+++ racket/10.rkt
@@ -1,3 +1,7 @@
+; 142913828922
+; 16.50user 1.34system 0:17.93elapsed 99%CPU (0avgtext+0avgdata 415548maxresident)k
+; 0inputs+0outputs (0major+160754minor)pagefaults 0swaps
+
 (display
   (foldl
     +
