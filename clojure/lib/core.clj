1d4e55af65a76dd2aef0ba00e368651936684088
Min Huang <mhuang@attinteractive.com>
2012-03-14 16:30:40 -0700

    Add clj-euler dep

--- clojure/lib/core.clj
+++ clojure/lib/core.clj
@@ -4,4 +4,4 @@
   :coordinates
     '[[org.clojure/math.numeric-tower "0.0.1"]
       [org.clojure/math.combinatorics "0.0.2"]
-      [com.lousycoder/clj-euler "0.0.1"]])
+      [com.lousycoder/clj-euler "2.0.1"]])
