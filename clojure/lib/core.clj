9fdf179cb9e1032c7dfaa67a8cc5fc35d095a520
Min Huang <min.huang@alumni.usc.edu>
2012-03-14 03:21:44 -0700

    Have this load the clj-euler stuff when it exists

--- clojure/lib/core.clj
+++ clojure/lib/core.clj
@@ -3,4 +3,5 @@
 (add-dependencies
   :coordinates
     '[[org.clojure/math.numeric-tower "0.0.1"]
-      [org.clojure/math.combinatorics "0.0.2"]])
+      [org.clojure/math.combinatorics "0.0.2"]
+      [com.lousycoder/clj-euler "0.0.1"]])
