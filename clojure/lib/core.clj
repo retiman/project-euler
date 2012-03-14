308c4e7b3f19d6167c25462730ca976353815d9b
Min Huang <mhuang@attinteractive.com>
2012-03-14 16:39:08 -0700

    Grab clj-euler

--- clojure/lib/core.clj
+++ clojure/lib/core.clj
@@ -1,6 +1,10 @@
 (use '[cemerick.pomegranate :only (add-dependencies)])
 
 (add-dependencies
+  :repositories
+    (merge
+      cemerick.pomegranate.aether/maven-central
+      {"clojars" "http://clojars.org/repo"})
   :coordinates
     '[[org.clojure/math.numeric-tower "0.0.1"]
       [org.clojure/math.combinatorics "0.0.2"]
