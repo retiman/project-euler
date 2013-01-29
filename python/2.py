22ac808428ce5381259d3e40cb5548c20ba2dfd3
Min Huang <min.huang@alumni.usc.edu>
2013-01-28 21:31:32 -0800

    Demonstration of using itertools

--- python/2.py
+++ python/2.py
@@ -1,6 +1,10 @@
-curr, succ, acc = 0, 1, 0
-while curr < 4000000:
-  curr, succ = succ, curr + succ
-  if curr % 2 == 0:
-    acc += curr
-print(acc)
+from itertools import ifilter, takewhile
+
+def fibs():
+    a, b = 0, 1
+    while True:
+        yield b
+        a, b = b, a + b
+
+xs = fibs()
+print sum(ifilter(lambda x: x % 2 == 0, takewhile(lambda x: x < 4000000, xs)))
