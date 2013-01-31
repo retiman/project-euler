afd815a89f174b2c50061d1cbb5c23900fdd1ae8
Min Huang <min.huang@alumni.usc.edu>
2013-01-30 22:48:10 -0800

    I feel like this is a much more common use case...

--- python/lib/core.py
+++ python/lib/core.py
@@ -1,2 +1,15 @@
 def compose(*funcs, **kfuncs):
-  return reduce(lambda f, g: lambda *args, **kaargs: f(g(*args, **kaargs)), funcs)
+    return reduce(lambda f, g: lambda *args, **kaargs: f(g(*args, **kaargs)), funcs)
+
+def irange(start, end = None, step = None):
+    if end == None:
+        end = start
+        return xrange(end + 1)
+    elif start >= end:
+        if step == None:
+            step = -1
+        return xrange(start, end - 1, step)
+    else:
+        if step == None:
+            step = 1
+        return xrange(start, end + 1, step)
