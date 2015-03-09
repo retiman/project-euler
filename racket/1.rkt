13fefee3b13b5bc79c374d9598ae0754b6a82dc3
Min Huang <min.huang@alumni.usc.edu>
2015-03-09 15:16:42 -0700

    Even shorter version of 1

--- racket/1.rkt
+++ racket/1.rkt
@@ -2,4 +2,4 @@
   (apply + (filter (lambda (n)
                      (or (zero? (modulo n 3))
                          (zero? (modulo n 5))))
-                   (build-list 1000 values))))
+                   (range 1000))))
