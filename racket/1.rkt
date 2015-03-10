2fdcf6c870d57ff3b2814df42530883c4e115e84
Min Huang <min.huang@alumni.usc.edu>
2015-03-10 14:35:34 -0700

    Update 1.rkt
    Even shorter.

--- racket/1.rkt
+++ racket/1.rkt
@@ -1,5 +1,4 @@
 (displayln
-  (apply + (filter (lambda (n)
-                     (or (zero? (modulo n 3))
-                         (zero? (modulo n 5))))
+  (apply + (filter (λ (n) (or (zero? (modulo n 3))
+                              (zero? (modulo n 5))))
                    (range 1000))))
