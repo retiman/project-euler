dfefdc853ce248ac79d5254ca5a1eab21e60ac31
Min Huang <min.huang@alumni.usc.edu>
2015-03-10 02:40:36 -0700

    Simpler #9 in Racket

--- racket/9.rkt
+++ racket/9.rkt
@@ -1,14 +1,5 @@
-(require racket/generator)
-
-(define g
-  (generator
-    ()
-    (for* ((a (in-range 1 333))
-           (b (in-range a (/ (- 1000 a) 2))))
-          (let ((c (- 1000 a b)))
-            (when (= (+ (* a a) (* b b))
-                     (* c c))
-              (yield (* a b c)))))))
-
-(display (g))
-(newline)
+(for* ((a (range 1 333))
+        (b (range a (/ (- 1000 a) 2))))
+      (let ((c (- 1000 a b)))
+        (when (= (+ (* a a) (* b b)) (* c c))
+            (displayln (* a b c)))))
