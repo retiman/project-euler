e2be3b38545685976222e0ac128cc70e0fc35c94
Min Huang <min.huang@alumni.usc.edu>
2015-03-10 16:46:44 -0700

    Simpler version of #11 in Racket

--- racket/11.rkt
+++ racket/11.rkt
@@ -1,56 +1,44 @@
-(define matrix
-  (list->vector
-    (map
-      (compose
-        list->vector
-        (lambda (s)
-          (map string->number (regexp-split #px" " s))))
-      (file->lines "../data/11.txt"))))
+(require math/matrix)
 
-(define (vget matrix i j)
-  (vector-ref (vector-ref matrix i) j))
+(define (line->vector line)
+  (for/vector ((e (regexp-split #px" " line)))
+    (string->number e)))
+
+(define m
+  (vector*->matrix
+    (for/vector ((line (file->lines "../data/11.txt")))
+      (line->vector line))))
 
 (define (horizontal-product i j)
   (with-handlers
-    ((exn:fail?
-       (lambda (e) 0)))
-    (foldl * 1
-      (map (lambda (k) (vget matrix i (+ j k)))
-           (stream->list (in-range 4))))))
+    ((exn:fail? (const 0)))
+    (for/product ((k (in-range 4)))
+                 (matrix-ref m i (+ j k)))))
 
 (define (vertical-product i j)
   (with-handlers
-    ((exn:fail?
-       (lambda (e) 0)))
-    (foldl * 1
-      (map (lambda (k) (vget matrix (+ i k) j))
-           (stream->list (in-range 4))))))
+    ((exn:fail? (const 0)))
+    (for/product ((k (in-range 4)))
+                 (matrix-ref m (+ i k) j))))
 
 (define (diagonal-up-product i j)
   (with-handlers
-    ((exn:fail?
-       (lambda (e) 0)))
-    (foldl * 1
-      (map (lambda (k) (vget matrix (- i k) (+ j k)))
-           (stream->list (in-range 4))))))
+    ((exn:fail? (const 0)))
+    (for/product ((k (in-range 4)))
+                 (matrix-ref m (- i k) (+ j k)))))
 
 (define (diagonal-down-product i j)
   (with-handlers
-    ((exn:fail?
-       (lambda (e) 0)))
-    (foldl * 1
-      (map (lambda (k) (vget matrix (+ i k) (+ j k)))
-           (stream->list (in-range 4))))))
+    ((exn:fail? (const 0)))
+    (for/product ((k (in-range 4)))
+                 (matrix-ref (+ i k) (+ j k)))))
 
-(display
-  (apply
-    max
+(displayln
+  (apply max
     (flatten
-      (for*/list
-        ((i (in-range 20))
-         (j (in-range 20)))
+      (for*/list ((i (in-range 20))
+                  (j (in-range 20)))
         (list (horizontal-product i j)
               (vertical-product i j)
               (diagonal-up-product i j)
               (diagonal-down-product i j))))))
-(newline)
