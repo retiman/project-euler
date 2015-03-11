3b9b83c5fe2723964a6559543b3cbb33a0853ce4
Min Huang <min.huang@alumni.usc.edu>
2015-03-10 18:33:53 -0700

    Simpler version of 12

--- racket/12.rkt
+++ racket/12.rkt
@@ -1,9 +1,6 @@
 (require "lib/core.rkt")
 (require "lib/number-theory.rkt")
 
-(define (f n i)
-  (if (> (tau n) 500)
-    n
-    (f (+ n i) (add1 i))))
-
-(displayln (f 3 3))
+(displayln
+  (letrec ((f (λ (n i) (if (> (τ n) 500) n (f (+ n i) (add1 i))))))
+    (f 3 3)))
