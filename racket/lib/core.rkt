46bd5611bd62309612c8499b595f1700caadac70
Min Huang <min.huang@alumni.usc.edu>
2015-03-10 17:12:41 -0700

    Number theory functions. Racket standard library has them, but what fun is it using them?

--- racket/lib/core.rkt
+++ racket/lib/core.rkt
@@ -1,14 +1,22 @@
 #lang racket
 
 (provide char->number
+         set-length
          stream-take
          stream-takef)
 
 (define (char->number c)
   (string->number (make-string 1 c)))
 
+(define (set-length s)
+  (sequence-length s))
+
 (define (stream-take s n)
   (for/list ((e s) (i (in-range n))) e))
 
 (define (stream-takef s f)
   (for/list ((e s) #:break (not (f e))) e))
+
+(define (memoize f)
+  (local ((define table (make-hash)))
+    (λ args (dict-ref! table args (λ () (apply f args))))))
---
+++ racket/lib/number-theory.rkt
@@ -0,0 +1,58 @@
+#lang racket
+
+(require "core.rkt")
+
+(define (coprime? m n)
+  (= (gcd m n) 1))
+
+(define (divides? m n)
+  (zero? (modulo m n)))
+
+(define divisors
+  (memoize
+    (λ (n)
+      (let* ((xs (filter (curry divides? n) (range 2 (add1 (integer-sqrt n)))))
+             (ys (map (curry quotient n) xs)))
+        (set-union (set 1 n) (list->set xs) (list->set ys))))))
+
+(define (modular-expt b e m)
+  (letrec ((f (λ (a b e)
+                (if (<= e 0)
+                  a
+                  (let ((t (if (= (bitwise-and e 1) 1) (modulo (* a b) m) a)))
+                    (f t (modulo (* b b) m) (arithmetic-shift e -1)))))))
+    (f 1 b e)))
+
+(define (tau n)
+  (set-length (divisors n)))
+
+(define τ tau)
+
+(define (sigma n)
+  (for/sum ((i (divisors n))) i))
+
+(define σ sigma)
+
+(define (prime? n)
+  (or (= n 2) (= (set-length (divisors n)) 2)))
+
+(define (prime-factors n)
+  (filter prime? (set->list (divisors n))))
+
+(define totient
+  (memoize
+    (λ (n)
+      (cond ((= n 1) 0)
+      (else (* n (for/product ((p (prime-factors n)))
+                              (- 1 (/ 1 p)))))))))
+
+(define phi totient)
+
+(define φ totient)
+
+(define ord
+  (memoize
+    (λ (a m)
+      (unless (coprime? a m) (raise-argument-error 'a "coprime to m" a))
+      (let ((ds (set->list (divisors (φ m)))))
+        (first (dropf ds (λ (n) (> (modular-expt a n m) 1))))))))
