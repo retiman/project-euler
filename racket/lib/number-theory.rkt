60d8ca288837f00be0d24d43b3e007f35e7ec3e5
Min Huang <min.huang@alumni.usc.edu>
2015-03-10 21:08:59 -0700

    Replace memoize with define/memo

--- racket/lib/number-theory.rkt
+++ racket/lib/number-theory.rkt
@@ -23,12 +23,10 @@
 (define (divides? m n)
   (zero? (modulo m n)))
 
-(define divisors
-  (memoize
-    (λ (n)
-      (let* ((xs (filter (curry divides? n) (range 2 (add1 (integer-sqrt n)))))
-             (ys (map (curry quotient n) xs)))
-        (set-union (set 1 n) (list->set xs) (list->set ys))))))
+(define/memo (divisors n)
+  (let* ((xs (filter (curry divides? n) (range 2 (add1 (integer-sqrt n)))))
+          (ys (map (curry quotient n) xs)))
+    (set-union (set 1 n) (list->set xs) (list->set ys))))
 
 (define factorial n
   (define (factorial* i acc)
@@ -45,12 +43,10 @@
                     (f t (modulo (* b b) m) (arithmetic-shift e -1)))))))
     (f 1 b e)))
 
-(define ord
-  (memoize
-    (λ (a m)
-      (unless (coprime? a m) (raise-argument-error 'a "coprime to m" a))
-      (let ((ds (set->list (divisors (φ m)))))
-        (first (dropf ds (λ (n) (> (modular-expt a n m) 1))))))))
+(define/memo (ord a m)
+  (unless (coprime? a m) (raise-argument-error 'a "coprime to m" a))
+  (let ((ds (set->list (divisors (φ m)))))
+    (first (dropf ds (λ (n) (> (modular-expt a n m) 1))))))))
 
 (define (prime? n)
   (or (= n 2) (= (set-length (divisors n)) 2)))
@@ -64,12 +60,10 @@
 (define (tau n)
   (set-length (divisors n)))
 
-(define totient
-  (memoize
-    (λ (n)
-      (cond ((= n 1) 0)
-      (else (* n (for/product ((p (prime-factors n)))
-                              (- 1 (/ 1 p)))))))))
+(define (totient n)
+  (cond ((= n 1) 0)
+  (else (* n (for/product ((p (prime-factors n)))
+                          (- 1 (/ 1 p)))))))
 
 (define phi totient)
 (define σ sigma)
