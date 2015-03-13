1066e56679458ddc1dbf2f7459b958b5bb857c6d
Min Huang <min.huang@alumni.usc.edu>
2015-03-13 14:03:32 -0700

    Problem #26 in Racket

---
+++ racket/26.rkt
@@ -0,0 +1,23 @@
+(require "lib/number-theory.rkt")
+
+(define numbers
+  (filter (λ (n) (and (not (divides? n 2)) (not (divides? n 5))))
+          (range 999 1 -1)))
+
+(define (ord* a m)
+  (define (f b lst)
+    (if (or (= b 1) (= (length lst) 1))
+      (car lst)
+      (f (modulo (* a b) m) (cdr lst))))
+  (f (modulo a m) (range 1 m)))
+
+(define (max-period best numbers)
+  (if (empty? numbers)
+    best
+    (let* ((n (first numbers))
+           (t (ord* 10 n)))
+      (cond ((= t (sub1 n)) (cons n t))
+            ((> t (cdr best)) (max-period (cons n t) (rest numbers)))
+            (else (max-period best (rest numbers)))))))
+
+(displayln (car (max-period (cons 0 0) numbers)))
