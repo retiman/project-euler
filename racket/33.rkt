ea08c03728d33261fb7bdd3604d61e1d40139bb3
Min Huang <min.huang@alumni.usc.edu>
2015-03-26 01:00:11 -0700

    Problem #33 in Racket

---
+++ racket/33.rkt
@@ -0,0 +1,35 @@
+(require "lib/core.rkt")
+
+(define (denominators n)
+  (define (denominators* digit start)
+    (flatten
+      (for/list ((i (in-range start 10)))
+        (list (list->integer* (list i digit))
+              (list->integer* (list digit i))))))
+  (let* ((lst (integer->list* n))
+         (a (car lst))
+         (b (cadr lst)))
+    (filter (curryr > n)
+            (distinct (append (denominators* a a) (denominators* b a))))))
+
+(define (cancellable? m n)
+  (let* ((m* (integer->list* m))
+         (n* (integer->list* n))
+         (v (/ m n))
+         (a (car m*))
+         (b (cadr m*))
+         (c (car n*))
+         (d (cadr n*)))
+    (cond ((ormap zero? (list a b c d)) #f)
+          ((= a c) (= v (/ b d)))
+          ((= a d) (= v (/ b c)))
+          ((= b c) (= v (/ a d)))
+          ((= b d) (= v (/ a c)))
+          (else #f))))
+
+(displayln
+  (denominator
+    (for*/product ((m (range 11 99))
+                  (n (denominators m))
+                  #:when (cancellable? m n))
+      (/ m n))))
