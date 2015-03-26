eb6d0375d64008aff6476ee0f716b9a8e9291d2c
Min Huang <min.huang@alumni.usc.edu>
2015-03-25 21:51:42 -0700

    Add comments

--- racket/32.rkt
+++ racket/32.rkt
@@ -21,6 +21,7 @@
     (list a* b* c*)))
 
 (define (check-pandigital-product a b c)
+  ; If abc forms a pandigital product, then bac forms one as well.
   (let ((p1 (list->number* (list a b c)))
         (p2 (list->number* (list b a c))))
     (when (and (not (hash-has-key? pandigital-products p1))
@@ -31,8 +32,13 @@
 (begin
   (for ((p (permutations (range 1 10))))
     (let ((n (list->number* p)))
-      (apply check-pandigital-product (pandigital-product-values n 2 3))
-      (apply check-pandigital-product (pandigital-product-values n 1 4))))
+      ; Check for products formed by a * bcde = fghi or abcd * e = fghi.
+      (apply check-pandigital-product (pandigital-product-values n 1 4))
+      ; Check for products formed by ab * cde = fghi or abc * de = fghi.
+      ; It's impossible to have a product with 5 digits because:
+      ; (* 9 876) and (* 98 76) don't have 5 digits, but those are the max
+      ; products you can get with the 4 remaining digits.
+      (apply check-pandigital-product (pandigital-product-values n 2 3))))
   (displayln
     (for/sum ((i (distinct (hash-values pandigital-products))))
       i)))
