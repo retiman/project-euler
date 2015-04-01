093bd164e4bb0f2de13afd8ba9b552d2e7179bbe
Min Huang <min.huang@alumni.usc.edu>
2015-04-01 15:26:32 -0700

    Problem #35 in Racket

---
+++ racket/35.rkt
@@ -0,0 +1,22 @@
+(require "lib/core.rkt")
+(require "lib/number-theory.rkt")
+
+(define primes
+  (filter (curryr < 1000000) (file->list "../data/primes.txt")))
+
+(define (rotate lst)
+  (append (cdr lst) (list (car lst))))
+
+(define (rotations n)
+  (define (loop lst acc)
+    (let ((lst* (rotate lst))
+          (acc* (if (empty? acc) (list lst) acc)))
+      (if (= (length lst*) (length acc*))
+        acc*
+        (loop lst* (append acc* (list lst*))))))
+  (map list->integer* (loop (integer->list* n) empty)))
+
+(define (circular? p)
+  (andmap prime? (rotations p)))
+
+(displayln (length (filter circular? primes)))
