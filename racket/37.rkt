4e054f5a9e5f3f4008a6142f4d2eb342b990c480
Min Huang <min.huang@alumni.usc.edu>
2015-07-01 23:26:08 -0700

    Project #37 in Racket

---
+++ racket/37.rkt
@@ -0,0 +1,44 @@
+(require "lib/core.rkt")
+(require "lib/number-theory.rkt")
+
+(define primes
+  (filter (curryr < 1000000) (file->list "../data/primes.txt")))
+
+(define limit 11)
+
+(define (string-drop n s)
+  ((compose list->string
+            (curryr drop n)
+            string->list) s))
+
+(define (string-take n s)
+  ((compose list->string
+            (curryr take n)
+            string->list) s))
+
+(define (truncate* n direction)
+  (let* ((s (number->string n))
+         (k (if (symbol=? direction 'left)
+              (string-drop 1 s)
+              (string-take (sub1 (string-length s)) s))))
+    (string->number k)))
+
+(define truncatable?
+  (case-lambda
+    ((n) (and (truncatable? n 'left) (truncatable? n 'right)))
+    ((n direction) (and (prime? n)
+                        (or (< n 10)
+                            (truncatable? (truncate* n direction)
+                                          direction))))))
+
+(define find-primes
+  (case-lambda
+    ((ps) (find-primes ps '()))
+    ((ps ts) (let ((p (car ps)))
+               (cond ((< p limit) (find-primes (cdr ps) ts))
+                     ((= (length ts) limit) ts)
+                     (else (if (truncatable? p)
+                             (find-primes (cdr ps) (cons p ts))
+                             (find-primes (cdr ps) ts))))))))
+
+(displayln (for/sum ((p (find-primes primes))) p))
