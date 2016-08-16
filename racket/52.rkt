7ff51f16d221eb438c2b361b83a546b4ef2ee8b1
Min Huang <min.huang@alumni.usc.edu>
2016-08-16 01:08:12 -0700

    Problem #52 in Racket

---
+++ racket/52.rkt
@@ -0,0 +1,18 @@
+(require srfi/41)
+(require "lib/core.rkt")
+
+(define (same-digits? a b . more)
+  (let* ((xs (append (list a) (list b) more))
+         (ys (map integer->list* xs)))
+    (andmap (curry set=? (car ys)) (cdr ys))))
+
+(define (products n)
+  (map (curry * n) (range 1 (add1 6))))
+
+(define xs
+  (stream-map products (stream-from 1)))
+
+(define ys
+  (stream-drop-while (λ (n) (not (apply same-digits? n))) xs))
+
+(displayln (car (stream-car ys)))
