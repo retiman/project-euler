7525a266fdc80a7e464ca15d2d703609e7b18ecd
Min Huang <min.huang@alumni.usc.edu>
2016-08-17 13:44:43 -0700

    Comments for #15

--- racket/15.rkt
+++ racket/15.rkt
@@ -1,5 +1,17 @@
+; Problem #15: https://projecteuler.net/problem=15
+
 (require "lib/number-theory.rkt")
 
+; Starting at the upper left, one must move down exactly 20 times, and right
+; exactly 20 times in order to reach the bottom right. Represent the downward
+; movement as a 'D' and the rightward movement as an 'R', then the number of
+; routes to the bottom right is equal to the number of permutations of a 'word'
+; with 20 D's and 20 R's.
+;
+; This can be viewed as a multiset permutation whose cardinality is the
+; multinomial coefficient.
+;
+; See https://en.wikipedia.org/wiki/Permutation#Permutations_of_multisets
 (displayln
   (let* ((n (factorial (+ 20 20)))
          (d (* (factorial 20) (factorial 20)))
