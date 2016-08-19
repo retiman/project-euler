4f30de752fa8c9534efd1f1edab6c42de93398c8
Min Huang <min.huang@alumni.usc.edu>
2016-08-19 14:19:52 -0700

    Use order fn defined in libs

--- racket/26.rkt
+++ racket/26.rkt
@@ -1,21 +1,16 @@
+; Problem #26: https://projecteuler.net/problem=26
+
 (require "lib/number-theory.rkt")
 
 (define numbers
   (filter (λ (n) (and (not (divides? 2 n)) (not (divides? 5 n))))
           (range 999 1 -1)))
 
-(define (ord* a m)
-  (define (loop b lst)
-    (if (or (= b 1) (= (length lst) 1))
-      (car lst)
-      (loop (modulo (* a b) m) (cdr lst))))
-  (loop (modulo a m) (range 1 m)))
-
 (define (max-period best numbers)
   (if (empty? numbers)
     best
     (let* ((n (first numbers))
-           (t (ord* 10 n)))
+           (t (ord 10 n)))
       (cond ((= t (sub1 n)) (cons n t))
             ((> t (cdr best)) (max-period (cons n t) (rest numbers)))
             (else (max-period best (rest numbers)))))))
