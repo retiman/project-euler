5453525248ff5fbd83a4978ae04ceb219505fff6
Min Huang <min.huang@alumni.usc.edu>
2016-08-18 19:12:07 -0700

    Use digits fn in lib

--- racket/25.rkt
+++ racket/25.rkt
@@ -1,11 +1,10 @@
+; Problem #25: https://projecteuler.net/problem=25
+
 (require "lib/core.rkt")
 (require "lib/number-theory.rkt")
-
-(define (number-length n)
-  (string-length (number->string n)))
 
 (displayln
   (for/last ((i (in-naturals))
              (n fibs)
-             #:break (>= (number-length n) 1000))
+             #:break (>= (digits n) 1000))
     (add1 i)))
