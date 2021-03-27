b9062ce5df7dfa69bee25df2747b69f23479b5df
Min Huang <min.huang@alumni.usc.edu>
2021-03-27 00:40:00 -0700

    Assert on the result in Java

--- java/P40.java
+++ java/P40.java
@@ -1,8 +1,8 @@
-public class P40 {
+public final class P40 {
   public static void main(String[] args) {
     StringBuilder sb = new StringBuilder();
     String s = null;
-    int product = 1;
+    int result = 1;
 
     for (int i = 1; sb.length() < 1000000; i++) {
       sb.append(String.valueOf(i));
@@ -12,9 +12,10 @@ public class P40 {
     for (int i = 1; i <= 1000000; i *= 10) {
       char c = s.charAt(i - 1);
       int j = Integer.parseInt(Character.valueOf(c).toString());
-      product *= j;
+      result *= j;
     }
 
-    System.out.println(product);
+    System.out.println(result);
+    assert result == 210;
   }
 }
