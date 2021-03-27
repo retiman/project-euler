c6924cf058f01a61b5d2584f71cfb2f1e4ad1f4b
Min Huang <min.huang@alumni.usc.edu>
2021-03-27 01:11:26 -0700

    Asserts on clojure result

--- clojure/4.clj
+++ clojure/4.clj
@@ -4,10 +4,13 @@
   (let [s (str n)]
     (= s (apply str (reverse s)))))
 
-(println
+(def result
   (reduce max
     (for [i (range 999 1 -1)
           j (range i   1 -1)
           :let [x (* i j)]
           :when (is-palindrome? x)]
       x)))
+
+(println result)
+(assert (= result 906609))
