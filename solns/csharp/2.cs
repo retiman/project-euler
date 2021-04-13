31a287bca94bd25aea9ff453a1820807ad823937
Min Huang <min.huang@alumni.usc.edu>
2021-04-13 00:27:05 -0700

    Adds a solution for #2 in C#

---
+++ solns/csharp/2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+static IEnumerable<long> Fibonacci()
+{
+    var current = 0;
+    var next = 1;
+    while (true)
+    {
+        yield return current;
+
+        var t = next;
+        next = current + next;
+        current = t;
+    }
+}
+
+var result = Fibonacci().TakeWhile(n => n < 4_000_000)
+        .Where(n => n%2 == 0)
+        .Sum();
+
+Console.WriteLine(result);
