c6905715211dfbb06a71b3045408e82c16ff54ad
Min Huang <min.huang@alumni.usc.edu>
2021-04-18 20:56:54 -0700

    Fixes whitespace for linq query

--- solns/csharp/1.cs
+++ solns/csharp/1.cs
@@ -4,8 +4,8 @@ using System.Linq;
 
 
 var ns = from n in Enumerable.Range(1, 999)
-    where n % 3 == 0 || n % 5 == 0
-    select n;
+         where n % 3 == 0 || n % 5 == 0
+         select n;
 var result = ns.Sum();
 
 Console.WriteLine(result);
