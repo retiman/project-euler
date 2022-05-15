105676c38e256e35c178902c3f72facd91fe2cc4
Min Huang <min.huang@alumni.usc.edu>
2022-05-15 00:10:42 +0000

    remove lint

--- solns/racket/9.rkt
+++ solns/racket/9.rkt
@@ -6,9 +6,9 @@
 ; a >= 333, there is no way for the inequality to hold). Furthermore, this
 ; constrains b to being less than (1000 - a) / 2.
 (define result 0)
-(for* ([a (range 1 333)]
-       [b (range a (/ (- 1000 a) 2))])
-  (let ([c (- 1000 a b)])
+(for* ((a (range 1 333))
+       (b (range a (/ (- 1000 a) 2))))
+  (let ((c (- 1000 a b)))
     (when (= (+ (* a a) (* b b)) (* c c))
         (set! result (* a b c)))))
 
