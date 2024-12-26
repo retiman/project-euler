b83c4730b62bc4c1b7f891216167034b8061124d
Min Huang <min.huang@alumni.usc.edu>
2024-12-25 18:58:21 -0800

    updates jest config

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   modulePaths: ['<rootDir>'],
-  setupFiles: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testTimeout: 1000,
