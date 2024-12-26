7e6c3dfea66013b02c030e77f2d7bc8af91e1a89
Min Huang <min.huang@alumni.usc.edu>
2024-12-25 22:47:41 -0800

    no python

--- solns/python/7.py
+++
@@ -1,9 +0,0 @@
-primes = 0
-for line in open("../data/primes.txt", "r"):
-    primes += 1
-    if primes == 10001:
-        result = int(line)
-        break
-
-print(result)
-assert result == 104743
