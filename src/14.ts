79db94165569bb054560c016361af050cc465336
Min Huang <min.huang@alumni.usc.edu>
2024-12-26 14:50:55 -0800

    adds14

--- src/14.ts
+++ src/14.ts
@@ -1,10 +1,12 @@
+// Computes the number that produces the longest Collatz chain under a given limit.
+//
 // See https://projecteuler.net/problem=14
-export function compute(limit: number) {
+export default function compute(limit: number) {
   let max = 0;
   let result = 0;
 
   for (let i = 1; i < limit; i++) {
-    const current = collatz(i);
+    const current = collatzLength(i);
     if (current > max) {
       max = current;
       result = i;
@@ -14,24 +16,20 @@ export function compute(limit: number) {
   return result;
 }
 
-function collatz(n: number): number {
-  const memo = new Map<number, number>();
+const memo = new Map<number, number>();
 
-  function len(n: number): number {
-    if (n === 1) {
-      return 0;
-    }
-
-    if (memo.has(n)) {
-      return memo.get(n)!;
-    }
-
-    const next = n % 2 === 0 ? n / 2 : 3 * n + 1;
-    const result = 1 + len(next);
-
-    memo.set(n, result);
-    return result;
+function collatzLength(n: number): number {
+  if (n === 1) {
+    return 0;
   }
 
-  return len(n);
+  if (memo.has(n)) {
+    return memo.get(n)!;
+  }
+
+  const next = n % 2 === 0 ? n / 2 : 3 * n + 1;
+  const result = 1 + collatzLength(next);
+
+  memo.set(n, result);
+  return result;
 }
