48bf07423906588fe66ddd1b34d8d202179be8b2
Min Huang <min.huang@alumni.usc.edu>
2024-12-26 13:35:12 -0800

    docs for 1

--- src/1.ts
+++ src/1.ts
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+// Computes the sum of all the multiples of 3 or 5 below the given limit.
+//
 // See https://projecteuler.net/problem=1
 export function compute(limit: number) {
   return _.range(limit)
