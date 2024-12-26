909bbe7e90f243db75982b5b505ec962e35fd30f
Min Huang <min.huang@alumni.usc.edu>
2024-12-26 02:01:52 -0800

    17

--- src/16.ts
+++ src/16.ts
@@ -1,3 +1,6 @@
-export function compute(limit: number) {
-  return 0;
+import _ from 'lodash';
+
+export function compute(k: number, e: number) {
+  const s = BigInt(Math.pow(k, e)).toString();
+  return s.split('').reduce((a, b) => a + _.toNumber(b), 0);
 }
--- test/17.test.ts
+++ test/17.test.ts
@@ -2,6 +2,6 @@ import { compute } from '../src/17';
 
 describe('number letter counts', () => {
   test('problem 17', async () => {
-    expect(compute(1000)).toBe(1366);
+    expect(compute()).toBe(21124);
   });
 });
