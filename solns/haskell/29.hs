c87c0d4dd5691a72470e659e8cf9e5a2cba373cc
Min Huang <min.huang@alumni.usc.edu>
2024-12-26 02:36:47 -0800

    removed converted

--- solns/haskell/29.hs
+++
@@ -1,4 +0,0 @@
-import Data.List (nub)
-
-result = length (nub [a^b | a <- [2..100], b <- [2..100]])
-main = print result
--- solns/scala/29.scala
+++
@@ -1,8 +0,0 @@
-import scala.math.pow
-
-
-val ns = for (a <- 2 to 100; b <- 2 to 100) yield pow(a, b)
-val result = (Set() ++ ns).size
-
-println(result)
-assert(result == 9183)
