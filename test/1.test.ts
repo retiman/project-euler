497c15e55ff6c251c9876dec4a1c0afa72d5c5d0
Min Huang <min.huang@alumni.usc.edu>
2024-12-25 23:59:43 -0800

    skip them all

--- test/1.test.ts
+++ test/1.test.ts
@@ -1,7 +1,7 @@
 import { compute } from '../src/1';
 
 describe('multiples of 3 or 5', () => {
-  test('problem 1', async () => {
+  test.skip('problem 1', async () => {
     expect(compute(1000)).toBe(233168);
   });
 });
--- test/11.test.ts
+++ test/11.test.ts
@@ -1,7 +1,7 @@
 import { compute } from '../src/11';
 
 describe('summation of primes', () => {
-  test('problem 11', async () => {
+  test.skip('problem 11', async () => {
     expect(compute()).toBe(70600674);
   });
 });
