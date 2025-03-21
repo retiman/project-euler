ac25b550947963ccbb09b17834ad8cd05004e647
Min Huang <min.huang@alumni.usc.edu>
2025-03-21 12:08:07 -0700

    48

---
+++ project_euler/48.py
@@ -0,0 +1,22 @@
+# Problem 48: Self powers.
+#
+# Computes the last ten digits of the series 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+#
+# See https://projecteuler.net/problem=48
+from typing import cast
+from toolz import curried, pipe
+
+
+def run(n: int) -> int:
+    # This modulus will give us the last 10 digits.
+    m = 10**10
+    result = pipe(
+        range(1, n + 1),
+        curried.map(lambda b: pow(b, b, m)),
+        curried.reduce(lambda a, b: (a + b) % m),
+    )
+    return cast(int, result)
+
+
+def test_run():
+    assert run(1000) == 9110846700
