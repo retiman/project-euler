23cdf89b8bb8c69cc5d9ed54b79bbbcec3dca487
Min Huang <min.huang@alumni.usc.edu>
2025-03-21 16:50:12 -0700

    24

---
+++ project_euler/23.py
@@ -0,0 +1,25 @@
+# Problem 23: Non-Abundant Sums
+#
+# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+#
+# See https://projecteuler.net/problem=23
+from project_euler.common.divisors import sigma
+
+
+def run(limit: int) -> int:
+    abundants = list(filter(lambda n: sigma(n) - n> n, range(1, limit + 1)))
+    numbers = list(range(0, limit + 1))
+
+    for i in range(len(abundants)):
+        for j in range(i, len(abundants)):
+            k = abundants[i] + abundants[j]
+            if k <= limit:
+                numbers[k] = 0
+            else:
+                break
+
+    return sum(numbers)
+
+
+def test_run():
+    assert run(28123) == 4179871
