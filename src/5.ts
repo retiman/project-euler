c22777a30a7e2fab601ff18f2e3d2a11176092c1
Min Huang <min.huang@alumni.usc.edu>
2025-03-21 12:57:17 -0700

    5

--- src/5.ts
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-import lcm from './core/lcm';
-
-// Computes the smallest number that can be divided by each of the numbers from 1 to n without any remainder.
-//
-// See {@link https://projecteuler.net/problem=5}.
-export default function compute(n: number) {
-  return _.range(1, n + 1).reduce(lcm, 1);
-}
