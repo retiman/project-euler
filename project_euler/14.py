266d4d4fb17449230a246363e7e2ce5fb1b3f7cb
Min Huang <min.huang@alumni.usc.edu>
2025-03-21 15:47:47 -0700

    formatting

---
+++ project_euler/14.py
@@ -0,0 +1,33 @@
+# Problem 14: Longest Collatz Sequence
+#
+# Which starting number, under one million, produces the longest Collatz sequence?
+#
+# See https://projecteuler.net/problem=14
+import functools
+
+
+@functools.lru_cache(maxsize=None)
+def collatz_len(n: int) -> int:
+    if n == 1:
+        return 0
+
+    next = n // 2 if n % 2 == 0 else 3 * n + 1
+    return 1 + collatz_len(next)
+
+
+def run(limit: int) -> int:
+    max_len = 0
+    current_len = 0
+    result = 1
+
+    for i in range(1, limit):
+        current_len = collatz_len(i)
+        if current_len > max_len:
+            max_len = current_len
+            result = i
+
+    return result
+
+
+def test_run():
+    assert run(1_000_000) == 837799
