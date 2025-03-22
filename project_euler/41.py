d0ec5711b519ef375f3f0e8bdb4c3a0195ca7e5a
Min Huang <min.huang@alumni.usc.edu>
2025-03-21 20:14:16 -0700

    41

---
+++ project_euler/41.py
@@ -0,0 +1,26 @@
+# Problem 41: Pandigital Prime
+#
+# What is the largest n-digit pandigital prime that exists?
+#
+# See https://projecteuler.net/problem=41
+from itertools import permutations
+from sympy import isprime
+
+def tuple2int(t: tuple[str]) -> int:
+    return int("".join(t))
+
+
+def run():
+    max_p = 0
+
+    # 8 and 9 digit pandigital numbers are divisible by 3, so they cannot be prime.
+    for permutation in permutations("7654321"):
+        p = tuple2int(permutation)
+        if isprime(p):
+            max_p = max(max_p, p)
+
+    return max_p
+
+
+def test_run():
+    assert run() == 7652413
