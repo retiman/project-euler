c688c4d4b64dd73a469602a1251fdc5ce1039a57
Min Huang <min.huang@alumni.usc.edu>
2025-03-21 20:32:23 -0700

    76

---
+++ project_euler/70.py
@@ -0,0 +1,58 @@
+# Problem 70: Totient Permutation
+#
+# Find the value of n, 1 < n < 10^7, for which φ(n) is a permutation of n and the ratio n/φ(n) produces a minimum.
+#
+# See https://projecteuler.net/problem=70
+import math
+from sympy import primerange
+
+
+# This is a special totient function that optimizes the computation of totient for a semi-prime whose factors are p
+# and q.
+#
+# For a prime number, the totient is simply (sub1 p).  Because the totient is a multiplicative function,
+# totient(p * q) = totient(p) * totient(q).  Therefore, the totient of a semi-prime is simply (p - 1) * (q - 1).
+def totient(p: int, q: int) -> int:
+    return (p - 1) * (q - 1)
+
+
+# Checks if two positive integers' digits are permutations of each other.
+def is_permutation(m: int, n: int) -> bool:
+    return sorted(str(m)) == sorted(str(n))
+
+
+def semi_prime_pairs(limit: int) -> list[tuple[int, int]]:
+    n = 2 * (math.isqrt(limit) + 1)
+    ps = list(primerange(2, n))
+    pairs: list[tuple[int, int]] = []
+
+    for i in range(len(ps)):
+        for j in range(i, len(ps)):
+            p, q = ps[i], ps[j]
+            pairs.append((p, q))
+
+    return pairs
+
+
+def run(limit: int) -> int:
+    pairs = semi_prime_pairs(limit)
+    result = 0
+    min_ratio = math.inf
+
+    for p, q in pairs:
+        n = p * q
+        if n >= limit:
+            continue
+
+        phi = totient(p, q)
+        if is_permutation(n, phi):
+            ratio = n / phi
+            if ratio < min_ratio:
+                min_ratio = ratio
+                result = n
+
+    return result
+
+
+def test_run():
+    run(1_000_000) == 8319823
---
+++ project_euler/76.py
@@ -0,0 +1,23 @@
+# Problem 76: Counting Summations
+#
+# How many different ways can one hundred be written as a sum of at least two positive integers?
+#
+# See https://projecteuler.net/problem=76
+from functools import cache
+
+
+@cache
+def partition(k: int, n: int) -> int:
+    if k > n:
+        return 0
+    if k == n:
+        return 1
+    return partition(k + 1, n) + partition(k, n - k)
+
+
+def run(target: int) -> int:
+    return partition(1, target) - 1
+
+
+def test_run():
+    assert run(100) == 190569291
